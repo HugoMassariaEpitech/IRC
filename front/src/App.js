ea384241e134b49a252a53f988ca2d9068f6cb00
hugomassaria <hugo.massaria@epitech.eu>
2023-01-31 16:53:07 +0100

    API Connexion

--- front/src/App.js
+++ front/src/App.js
@@ -7,19 +7,23 @@ import {socket} from "./Service/Socket";
 
 function App() {
   const [isConnected, setConnected] = useState(false);
+  const [Banner, setBanner] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post("http://localhost:5000/api/auth/signin", {uid: "Massaria#1", password: "Sniperking59!"}).then((result) => {
+    axios.post("http://localhost:5000/api/auth/signin", {uid: event.target.getElementsByTagName("input")[0].value, password: event.target.getElementsByTagName("input")[1].value}).then((result) => {
       if (result.status == 200) {
-        socket.emit("Users", {[socket.id] : {nickName: "Hugo Massaria", avatarURL: "https://randomuser.me/api/portraits/men/8.jpg"}});
+        console.log(result);
+        socket.emit("Users", {[socket.id] : {nickname: result.data.nickname, avatar: result.data.avatar, uid: result.data.uid}});
         setConnected(true);
+      } else {
+        setBanner(result.data.message);
       }
     });
   }
   return (
     <>
       {
-        isConnected ? <Chat /> : <Connexion handleSubmit={handleSubmit}/>
+        isConnected ? <Chat /> : <Connexion handleSubmit={handleSubmit} Banner={Banner}/>
       }
     </>
   );
